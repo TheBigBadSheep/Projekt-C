d82d973cb167262090ba88aa112cc45f60804ea2
BigBadSheep <j.antich11@gmai.com>
2023-05-18 18:15:05 +0200

    Commit 18/05/23

--- pages/calendar/index.vue
+++ pages/calendar/index.vue
@@ -3,30 +3,14 @@
       <header class="flex justify-center ">
         <p class="current-date"></p>
         <div class="flex p-4 font-medium">
-          <button id="prev" @click="changeMonthLeft" class="p-4">Left</button>
-          <button id="next" @click="changeMonthRight" class="p-4">Right</button>
-          <button id="jumpBack" @click="jumpToCurrentMonthDate" class="p-4"> Check</button>
-          <div class="p-4"> {{currentYear}} </div>
+          <button id="prev" @click="changeMonthLeft" class="p-4">Prev. Month</button>
+          <button id="next" @click="changeMonthRight" class="p-4">Next Month</button>
+          <button id="jumpBack" @click="jumpToCurrentMonthDate" class="p-4"> Current Month </button>
+          <div class="p-4"> {{currentMonth}} / {{currentYear}} </div>
         </div>
       </header>
-    <!--  <div class="calendar">
-        <ul class="weeks">
-          <li>Sun</li>
-          <li>Mon</li>
-          <li>Tue</li>
-          <li>Wed</li>
-          <li>Thu</li>
-          <li>Fri</li>
-          <li>Sat</li>
-        </ul>
-        <NuxtLink :to="'/calendar/' + urlSlug">
-          <ul class="days"></ul>
-        </NuxtLink>  
-        <ul @click="printmything(3)" class="days"></ul>
-        <div v-for="index in calendarDays">{{index}}</div>
-      </div> -->
       <div class="grid grid-cols-7 space-x-5 ">
-        <div v-for="index in monthy" :key=index class="text-center">
+        <div v-for="index in monthDays" :key=index class="text-center">
           <NuxtLink :to="'/calendar/' + index + '-' + currentMonth + '-' + currentYear"  >
             <div class="py-5 hover:bg-green-100 rounded-lg flex flex-col" :class="(index === currentDay && currentYear === savedDate[2] && currentMonth === savedDate[1]) ? 'bg-blue-500' : ''"
             @click="saveDateInStore(index + '-' + currentMonth + '-' + currentYear)"  >
@@ -61,12 +45,11 @@
           calendarDays: null,
           daysInMonth: null,
           storeDates: null,
-          lalalal: null
         }
       },
   
       computed: {
-        monthy(){
+        monthDays(){
           return this.daysInMonth
         },
         
@@ -74,43 +57,13 @@
   
       methods: {
         renderCalendar () {
-
-          // JS -> didnt work out as I wanted TODO: Remake with vue
-          ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-          /*
-          let firstDayofMonth = new Date(this.currentYear, this.currentMonth, 1).getDay(), // getting first day of month
-            lastDateofMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate(), // getting last date of month
-            lastDayofMonth = new Date(this.currentYear, this.currentMonth, lastDateofMonth).getDay(), // getting last day of month
-            lastDateofLastMonth = new Date(this.currentYear, this.currentMonth, 0).getDate(); // getting last date of previous month
-            let liTag = "";
-            let tagli 
-
-            for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-                liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-            }
-
-            for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-                // adding active class to li if the current day, month, and year matched
-                let isToday = i === this.date.getDate() && this.currentMonth === new Date().getMonth() 
-                            && this.currentYear === new Date().getFullYear() ? "active" : "";
-                liTag += `<li class="${isToday}">${i}</li>`;
-            }
-
-            for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-                liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-            }
-            this.currentDate.innerText = `${this.months[this.currentMonth]} ${this.currentYear}`; // passing current mon and yr as currentDate text
-            this.calendarDays.innerHTML = liTag;
-           // console.log(this.calendarDays)
-           */
-           /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+           // Let calendar know how many days each month has
            let x
            if ( this.currentMonth === 4 || this.currentMonth === 6 || this.currentMonth === 9 || this.currentMonth === 11) {
             x = 30
           }else{
             x = 31
           }
-           //if ( this.currentMonth === 1 || 3 || 5 || 7 || 8 || 10 || 12) x = 31
            if ( this.currentMonth === 2 ) x = 28
            this.daysInMonth = x
         },
@@ -158,16 +111,7 @@
         }
       },
       mounted(){
-      //  this.calendarDays = document.querySelector(".days"),
-      //  this.currentDate = document.querySelector(".current-date"),
-
         this.date = new Date()
-      //  this.currentYear = this.date.getFullYear()
-      //  this.currentMonth = this.date.getMonth()
-
-      //  const currentDay = document.getElementsByClassName('active')[0].innerHTML
-      //  this.savedDate = [currentDay, this.currentMonth, this.currentYear]      //Saving day aswell as month/year to use in the router link
-      //  this.urlSlug = currentDay + '-' + (this.currentMonth + 1)
 
         var today = new Date();
         var dd = today.getDate();
