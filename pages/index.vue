9998a3a1d91d8f8a6d57e0623733ce05403e0976
BigBadSheep <j.antich11@gmai.com>
2023-05-30 20:32:58 +0200

    new start page & small page transition

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="relative h-full">
-    <div class="-z-10 absolute flex justify-center w-full bg-ToDo-Green h-56 -mt-2 text-white text-4xl shadow-inner-xl">
-      <span class="h-10 mt-9">T</span>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-10 mt-9 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <div class="h-screen">
+    <div class="flex justify-center items-center w-full bg-ToDo-Green h-full text-white text-4xl shadow-inner-xl">
+      <div class="flex justify-center items-center group  h-auto">
+      <span class="h-10 mt-9 group-hover:opacity-0 transform duration-300 ease-in-out">T</span>
+      <nuxt-link to="/calendar"> 
+      <div class="hover:rotate-180 transform duration-500 ease-in-out h-80"> 
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-80 mt-9 w-80 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
-      <span class="h-10 mt-9">DO</span>
-    </div>
-    <div class="z-10 sticky flex flex-col w-full">
-      <div class="w-11/12 lg:w-1/2 sm:place-self-center">
-        <ToDoList class="mt-28"></ToDoList>
+      </div>
+      </nuxt-link>
+      <span class="h-10 mt-9 group-hover:opacity-0 transform duration-300 ease-in-out">DO</span>
       </div>
     </div>
   </div>
@@ -21,6 +22,8 @@
 import ToDoList from "../components/ToDoList.vue"
 
 export default {
+    transition: "startPage",
+    
     components: {
       ToDoList
     },
@@ -43,6 +46,14 @@ export default {
 </script>
 
 <style>
-
-
+.startPage-enter-active,
+.startPage-leave-active {
+  transition-property: opacity;
+  transition-timing-function: ease-in-out;
+  transition-duration: 300ms;
+}
+.startPage-enter,
+.startPage-leave-to {
+  opacity: 0;
+}
 </style>
