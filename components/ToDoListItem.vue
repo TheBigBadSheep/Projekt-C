0e2b2855448a995f09d6c26339a01029e201078a
TheBigBadSheep <j.antich11@gmail.com>
2023-07-18 23:56:21 +0200

    feat(PC-31):Delete-Image-button

--- components/ToDoListItem.vue
+++ components/ToDoListItem.vue
@@ -143,7 +143,22 @@
         :class="item.isChecked ? 'opacity-20' : 'opacity-80'"
         class="h-full w-full rounded-full object-cover border-2 border-white shadow-md hover:opacity-100 transition ease-in-out duration-200"
       />
+      
     </button>
+    <button @click="removeTaskImage" v-if="item.image" class="h-4 w-4 ml-7 z-50 absolute left-20 -bottom-8 text-ToDo-gradient-purple border-white bg-white rounded-full ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-4 h-4 "
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
   </div>
 </template>
 
